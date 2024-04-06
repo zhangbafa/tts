98d03c6962f30dcfcf1d509e4e1ce59c9fefdbe8
zhangbafa <youdan2020@qq.com>
2024-04-06 22:05:21 +0800

    44

--- server/routes/files/[name].get.js
+++ server/routes/files/[name].get.js
@@ -5,9 +5,9 @@ export default defineEventHandler(async (event) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
 
-  let uploadDir = path.join(__dirname, "../public/tts");
+  let uploadDir = path.join(__dirname, "/tmp/tts");
   if (process.env.NODE_ENV === 'development') {
-      uploadDir = path.join(__dirname, "../../public/tts");
+      uploadDir = path.join(__dirname, "/tmp/tts");
   }
 
   const filePath = path.join(uploadDir, event.context.params.name);
