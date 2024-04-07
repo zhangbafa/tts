fdafa17348dd0c481f59c5dbfd4732de54ecf521
zhangbafa <youdan2020@qq.com>
2024-04-07 14:17:07 +0800

    添加效果展示

--- components/Example.vue
+++ components/Example.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="mx-auto w-11/12 md:w-4/6 mb-20 border-b pb-10">
-        <div class="text-3xl text-gray-080 text-center font-bold mt-20 mb-20">效果展示</div>
+        <div class="text-3xl text-gray-080 text-center font-bold mt-20 ">效果展示</div>
+        <div class="text-sm text-gray-400 m-4 mb-15 text-center">zh-CN-XiaoxiaoMultiligualNeural</div>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div class="text-gray-500 text-sm">
                 但我现在对这个职业的热爱还是非常的，呵呵,非常的，嗯,怎么说呢？日月可鉴的，哈哈，嗯还是希望可以把这个职业做下去或者做这个声音相关领域的工作，嗯，就是把自己的优势发挥的大一点，尽可能能用到自己擅长的东西，而不是说为了工作，为了挣钱而工作。
